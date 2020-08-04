fc59e12133abbce07a789bdf278cb4b92f8bf18e
JEACO <jeaco@orsted.dk>
2020-08-04 16:45:33 +0200

    black

--- tables/writers/csv_writer.py
+++ tables/writers/csv_writer.py
@@ -6,7 +6,12 @@ from pathlib import Path
 from ..pdtable import Table
 
 
-def write_csv(tables: Union[Table, Iterable[Table], TableBundle], out: Union[str, Path, TextIO], sep: str = ';', na_rep: str = '-'):
+def write_csv(
+    tables: Union[Table, Iterable[Table], TableBundle],
+    out: Union[str, Path, TextIO],
+    sep: str = ";",
+    na_rep: str = "-",
+):
     """Writes one or more tables to CSV file or stream.
 
     Writes table blocks in CSV format to a file or stream. Values are formatted to comply with the StarTable standard where necessary and possible; 
@@ -26,57 +31,64 @@ def write_csv(tables: Union[Table, Iterable[Table], TableBundle], out: Union[str
     if isinstance(tables, Table):
         # For convenience, pack single table in an iterable
         tables = [tables]
-    
+
     # TODO Surely there's a better pattern than this? This one forces duplicate code... which I've packed away in a dummy function
     if isinstance(out, str) or isinstance(out, Path):
         # out is a file path. Open a stream and close it when done.
-        with open(out, 'w') as f:
+        with open(out, "w") as f:
             _tables_to_csv_propagator(tables, f, sep, na_rep)
     else:
-        # out is a stream opened by caller. Leave it open. 
+        # out is a stream opened by caller. Leave it open.
         _tables_to_csv_propagator(tables, out, sep, na_rep)
 
 
-def _tables_to_csv_propagator(tables: Union[Table, Iterable[Table], TableBundle], stream: TextIO, sep: str = ';', na_rep: str = '-') -> None:
+def _tables_to_csv_propagator(
+    tables: Union[Table, Iterable[Table], TableBundle],
+    stream: TextIO,
+    sep: str = ";",
+    na_rep: str = "-",
+) -> None:
     """Serves no purpose other than housing the duplicate code from the isinstance(out,...) conditional above
     """
     for table in tables:
         _table_to_csv(table, stream, sep, na_rep)
 
 
-def _table_to_csv(table: Table, stream: TextIO, sep: str = ';', na_rep: str = '-') -> None:
+def _table_to_csv(
+    table: Table, stream: TextIO, sep: str = ";", na_rep: str = "-"
+) -> None:
     """Writes a single Table to stream as CSV. 
     """
     units = table.units
-    stream.write(f'**{table.name}\n')
-    stream.write(' '.join(str(x) for x in table.metadata.destinations) + '\n')
-    stream.write(sep.join(str(x) for x in table.column_names) + '\n')
-    stream.write(sep.join(str(x) for x in units) + '\n')
+    stream.write(f"**{table.name}\n")
+    stream.write(" ".join(str(x) for x in table.metadata.destinations) + "\n")
+    stream.write(sep.join(str(x) for x in table.column_names) + "\n")
+    stream.write(sep.join(str(x) for x in units) + "\n")
     for row in table.df.itertuples(index=False, name=None):
         # TODO: apply format string specified in ColumnMetadata
-        stream.write(sep.join(_format_row_elements(row, units, na_rep)) + '\n')
-    stream.write('\n')
+        stream.write(sep.join(_format_row_elements(row, units, na_rep)) + "\n")
+    stream.write("\n")
 
 
-def _format_row_elements(row: Iterable, col_units: Iterable, na_rep: str = '-'):
+def _format_row_elements(row: Iterable, col_units: Iterable, na_rep: str = "-"):
     """Formats row elements to comply with the StarTable standard when written to CSV. 
     """
     for col, (val, unit) in enumerate(zip(row, col_units)):
-        if unit != 'text' and pd.isna(val):
+        if unit != "text" and pd.isna(val):
             # Format NaN-like things, except leave them be in text columns
             yield na_rep
-        elif unit == 'onoff':
+        elif unit == "onoff":
             # Format obvious booleans as 0's and 1's
             if val in [True, 1]:
-                yield '1'
+                yield "1"
             elif val in [False, 0]:
-                yield '0'
+                yield "0"
             else:
                 # If it isn't an obvious boolean, leave it be
                 yield str(val)
-        elif unit == 'text' and val == '' and col == 0:
+        elif unit == "text" and val == "" and col == 0:
             # Prevent illegal empty string in first column
-            yield '-'  # some reasonable placeholder non-empty string
+            yield "-"  # some reasonable placeholder non-empty string
         else:
             # Leave everything else be as it is
             yield str(val)
--- tables/writers/tests/test_csv_writer.py
+++ tables/writers/tests/test_csv_writer.py
@@ -9,37 +9,50 @@ from ..csv_writer import _table_to_csv, _format_row_elements
 
 
 def test__format_row_elements():
-    units = ['text', 'km', 'datetime', 'onoff']
+    units = ["text", "km", "datetime", "onoff"]
     # Standard stuff
-    assert list(_format_row_elements(
-        ('foo', 123, pd.to_datetime('2020-08-04 08:00'), True), units)) \
-        == ['foo', '123', '2020-08-04 08:00:00', '1']
+    assert list(
+        _format_row_elements(
+            ("foo", 123, pd.to_datetime("2020-08-04 08:00"), True), units
+        )
+    ) == ["foo", "123", "2020-08-04 08:00:00", "1"]
 
     # With NaN-like things
-    assert list(_format_row_elements(
-        ('foo', float('nan'), pd.NaT, 1), units)) \
-        == ['foo', '-', '-', '1']
-    
+    assert list(_format_row_elements(("foo", float("nan"), pd.NaT, 1), units)) == [
+        "foo",
+        "-",
+        "-",
+        "1",
+    ]
+
     # Specifying how NaN-like things should be displayed
-    assert list(_format_row_elements(
-        ('foo', float('nan'), pd.NaT, False), units, na_rep='NaN')) \
-        == ['foo', 'NaN', 'NaN', '0']
+    assert list(
+        _format_row_elements(("foo", float("nan"), pd.NaT, False), units, na_rep="NaN")
+    ) == ["foo", "NaN", "NaN", "0"]
 
     # Empty strings: replace illegal in first column, leave others
-    assert list(_format_row_elements(('','',float('nan')), ['text', 'text', 'text'])) \
-        == ['-', '', 'nan']
+    assert list(
+        _format_row_elements(("", "", float("nan")), ["text", "text", "text"])
+    ) == ["-", "", "nan"]
 
 
 def test__table_to_csv():
-    t = Table(name='foo')
-    t['place'] = ['home', 'work', 'beach', 'wonderland']
-    t.add_column('distance', list(range(3)) + [float('nan')], 'km')
-    t.add_column('ETA', pd.to_datetime(['2020-08-04 08:00', '2020-08-04 09:00', '2020-08-04 17:00', pd.NaT]), 'datetime')
-    t.add_column('is_hot', [True, False, True, False], 'onoff')
+    t = Table(name="foo")
+    t["place"] = ["home", "work", "beach", "wonderland"]
+    t.add_column("distance", list(range(3)) + [float("nan")], "km")
+    t.add_column(
+        "ETA",
+        pd.to_datetime(
+            ["2020-08-04 08:00", "2020-08-04 09:00", "2020-08-04 17:00", pd.NaT]
+        ),
+        "datetime",
+    )
+    t.add_column("is_hot", [True, False, True, False], "onoff")
 
     with io.StringIO() as out:
         _table_to_csv(t, out)
-        assert out.getvalue() == dedent("""\
+        assert out.getvalue() == dedent(
+            """\
             **foo
             all
             place;distance;ETA;is_hot
@@ -49,23 +62,31 @@ def test__table_to_csv():
             beach;2.0;2020-08-04 17:00:00;1
             wonderland;-;-;0
 
-            """)
+            """
+        )
 
 
 def test_write_csv__writes_two_tables():
-    t = Table(name='foo')
-    t['place'] = ['home', 'work', 'beach', 'wonderland']
-    t.add_column('distance', list(range(3)) + [float('nan')], 'km')
-    t.add_column('ETA', pd.to_datetime(['2020-08-04 08:00', '2020-08-04 09:00', '2020-08-04 17:00', pd.NaT]), 'datetime')
-    t.add_column('is_hot', [True, False, True, False], 'onoff')    
+    t = Table(name="foo")
+    t["place"] = ["home", "work", "beach", "wonderland"]
+    t.add_column("distance", list(range(3)) + [float("nan")], "km")
+    t.add_column(
+        "ETA",
+        pd.to_datetime(
+            ["2020-08-04 08:00", "2020-08-04 09:00", "2020-08-04 17:00", pd.NaT]
+        ),
+        "datetime",
+    )
+    t.add_column("is_hot", [True, False, True, False], "onoff")
 
-    t2 = Table(name='bar')
-    t2.add_column('digit',[1,6,42],'-')
-    t2.add_column('spelling', ['one','six','forty-two'], 'text')
+    t2 = Table(name="bar")
+    t2.add_column("digit", [1, 6, 42], "-")
+    t2.add_column("spelling", ["one", "six", "forty-two"], "text")
 
     with io.StringIO() as out:
         write_csv([t, t2], out)
-        assert out.getvalue() == dedent("""\
+        assert out.getvalue() == dedent(
+            """\
             **foo
             all
             place;distance;ETA;is_hot
@@ -83,17 +104,19 @@ def test_write_csv__writes_two_tables():
             6;six
             42;forty-two
 
-            """)
+            """
+        )
 
 
 def test_write_csv__writes_one_table():
-    t2 = Table(name='bar')
-    t2.add_column('digit',[1,6,42],'-')
-    t2.add_column('spelling', ['one','six','forty-two'], 'text')
+    t2 = Table(name="bar")
+    t2.add_column("digit", [1, 6, 42], "-")
+    t2.add_column("spelling", ["one", "six", "forty-two"], "text")
 
     with io.StringIO() as out:
         write_csv(t2, out)
-        assert out.getvalue() == dedent("""\
+        assert out.getvalue() == dedent(
+            """\
             **bar
             all
             digit;spelling
@@ -102,4 +125,5 @@ def test_write_csv__writes_one_table():
             6;six
             42;forty-two
 
-            """)        
+            """
+        )
