99683ff9f20edd140c298bf02aa47721e7aa3f4e
JEACO <jeaco@orsted.dk>
2020-08-04 15:36:40 +0200

    Add write_csv()

--- tables/writers/csv_writer.py
+++ tables/writers/csv_writer.py
@@ -1,14 +1,51 @@
+from tables.store import TableBundle
 import pandas as pd
-from typing import Iterable, TextIO
+from typing import Iterable, TextIO, Union
+from pathlib import Path
 
 from ..pdtable import Table
 
 
-def _table_to_csv(table: Table, stream: TextIO, sep: str = ';', nan: str = '-') -> None:
+def write_csv(tables: Union[Table, Iterable[Table], TableBundle], out: Union[str, Path, TextIO], sep: str = ';', na_rep: str = '-'):
+    """Writes one or more tables to CSV file or stream.
+
+    Writes table blocks in CSV format to a file or stream. Values are formatted to comply with the StarTable standard where necessary and possible; 
+    otherwise they are simply str()'ed. 
+
+    Args:
+        tables: 
+            Table(s) to write. Can be a single Table or an iterable of Tables. 
+        out:
+            File path or text stream to which to write. 
+        sep:
+            Optional; CSV field delimiter.
+        na_rep:
+            Optional; String representation of missing values (NaN, None, NaT). If overriding the default '-', it is recommended to use another value compliant with the StarTable standard.
     """
-    Write Table to stream in CSV format.
-    :param stream: Output stream, usually something returned by open()
-    :param sep: CSV column separator character
+
+    if isinstance(tables, Table):
+        # For convenience, pack single table in an iterable
+        tables = [tables]
+    
+    # TODO Surely there's a better pattern than this? This one forces duplicate code... which I've packed away in a dummy function
+    if isinstance(out, str) or isinstance(out, Path):
+        # out is a file path. Open a stream and close it when done.
+        with open(out, 'w') as f:
+            _tables_to_csv_propagator(tables, f, sep, na_rep)
+    else:
+        # out is a stream opened by caller. Leave it open. 
+        _tables_to_csv_propagator(tables, out, sep, na_rep)
+
+
+def _tables_to_csv_propagator(tables: Union[Table, Iterable[Table], TableBundle], stream: TextIO, sep: str = ';', na_rep: str = '-') -> None:
+    """Serves no purpose other than housing the duplicate code from the isinstance(out,...) conditional above
+    """
+    for table in tables:
+        _table_to_csv(table, stream, sep, na_rep)
+
+
+def _table_to_csv(table: Table, stream: TextIO, sep: str = ';', na_rep: str = '-') -> None:
+    """Writes a single Table to stream as CSV. 
     """
     units = table.units
     stream.write(f'**{table.name}\n')
@@ -17,15 +54,17 @@ def _table_to_csv(table: Table, stream: TextIO, sep: str = ';', nan: str = '-') 
     stream.write(sep.join(str(x) for x in units) + '\n')
     for row in table.df.itertuples(index=False, name=None):
         # TODO: apply format string specified in ColumnMetadata
-        stream.write(sep.join(_format_row_elements(row, units, nan)) + '\n')
+        stream.write(sep.join(_format_row_elements(row, units, na_rep)) + '\n')
     stream.write('\n')
 
 
-def _format_row_elements(row: Iterable, col_units: Iterable, nan: str = '-'):
+def _format_row_elements(row: Iterable, col_units: Iterable, na_rep: str = '-'):
+    """Formats row elements to comply with the StarTable standard when written to CSV. 
+    """
     for col, (val, unit) in enumerate(zip(row, col_units)):
         if unit != 'text' and pd.isna(val):
             # Format NaN-like things, except leave them be in text columns
-            yield nan
+            yield na_rep
         elif unit == 'onoff':
             # Format obvious booleans as 0's and 1's
             if val in [True, 1]:
