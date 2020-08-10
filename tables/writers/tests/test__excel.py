bd97db01056c8e4f831710bb960f269c9c4ef610
JEACO <jeaco@orsted.dk>
2020-08-10 14:24:27 +0200

    Properly test write_excel()

--- tables/writers/tests/test__excel.py
+++ tables/writers/tests/test__excel.py
@@ -1,5 +1,7 @@
+import datetime
 import pandas as pd
 import openpyxl
+from pytest import approx
 
 try:
     from openpyxl.worksheet.worksheet import Worksheet as OpenpyxlWorksheet
@@ -45,9 +47,7 @@ def test__append_table_to_openpyxl_worksheet():
     assert [ws.cell(r, 4).value for r in range(5, 9)] == [1, 0, 1, 0]
 
 
-def test_write_excel__doesnt_crash(tmp_path):
-    # TODO Not sure how to test for real, except by round-trip; but no read_excel() implementation yet
-
+def test_write_excel(tmp_path):
     # Make a couple of tables
     t = Table(name="foo")
     t["place"] = ["home", "work", "beach", "wonderland"]
@@ -63,7 +63,28 @@ def test_write_excel__doesnt_crash(tmp_path):
     t2.add_column("digit", [1, 6, 42], "-")
     t2.add_column("spelling", ["one", "six", "forty-two"], "text")
 
-    # Just test that write_excel doesn't crash
+    # Write tables to workbook, save, and re-load
     out_path = tmp_path / "foo.xlsx"
     write_excel([t, t2], out_path)
+    wb = openpyxl.load_workbook(out_path)
+    ws = wb.active
+
+    # Assert loaded worksheet looks as expected:
+    # - table header by row
+    assert ws["A1"].value == "**foo"
+    assert ws["A2"].value == "all"
+    assert [ws.cell(3, c).value for c in range(1, 5)] == ["place", "distance", "ETA", "is_hot"]
+    assert [ws.cell(4, c).value for c in range(1, 5)] == ["text", "km", "datetime", "onoff"]
+
+    # - table data by column
+    assert [ws.cell(r, 1).value for r in range(5, 9)] == ["home", "work", "beach", "wonderland"]
+    assert [ws.cell(r, 2).value for r in range(5, 9)] == [0, 1, 2, "-"]
+    for r, d in zip(range(5,8), pd.to_datetime(["2020-08-04 08:00", "2020-08-04 09:00", "2020-08-04 17:00"])):
+        # workaround openpyxl bug: https://foss.heptapod.net/openpyxl/openpyxl/-/issues/1493
+        # openpyxl adds a spurious microsecond to some datetimes.
+        assert abs(ws.cell(r, 3).value - d) <= datetime.timedelta(microseconds=1)
+    assert ws.cell(8, 3).value == "-"
+    assert [ws.cell(r, 4).value for r in range(5, 9)] == [1, 0, 1, 0]
+
+    # Teardown
     out_path.unlink()
