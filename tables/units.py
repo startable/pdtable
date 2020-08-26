61cdfb4f77a4947d24fc3fd6c195e57f1acb4420
thp-mac <thpetsen@gmail.com>
2020-08-26 11:02:14 +0200

    unit_policy: added column.name

--- tables/units.py
+++ tables/units.py
@@ -5,7 +5,7 @@ from .pdtable import Table as TableType
 
 class UnitPolicy(ABC):
     @abstractmethod
-    def convert_value_to_base(self, value, unit: str) -> Tuple[Any, str]:
+    def convert_value_to_base(self, value, unit: str, column_name: str) -> Tuple[Any, str]:
         """
         Convert a value, unit - tuple to base unit
         """
@@ -16,7 +16,7 @@ def normalize_table_in_place(unit_policy: UnitPolicy, table: TableType):
     """Apply unit policy, modifying table in-place"""
     for column in table.column_proxies:
         unit = column.unit
-        new_values, new_unit = unit_policy.convert_value_to_base(column.values, unit)
+        new_values, new_unit = unit_policy.convert_value_to_base(column.values, unit, column.name)
         if not unit == new_unit:
             column.values = new_values
             column.unit = new_unit
