33f12031fd0aff5c6ec5695a84d70de8af028585
thp-mac <thpetsen@gmail.com>
2020-08-31 12:50:41 +0200

    added test that illustrates UnitPolicy's use of table.name and column.name

--- tables/test/test_units.py
+++ tables/test/test_units.py
@@ -7,7 +7,7 @@ from textwrap import dedent
 
 
 class MockUnitPolicy(UnitPolicy):
-    def convert_value_to_base(self, value, unit: str, column_name: str) -> Tuple[Any, str]:
+    def convert_value_to_base(self, value, unit: str) -> Tuple[Any, str]:
         if unit == "mm":
             return value * 1e-3, "m"
         else:
@@ -20,8 +20,8 @@ def unit_policy() -> UnitPolicy:
 
 
 def test_convert_values(unit_policy):
-    assert unit_policy.convert_value_to_base(1, "mm", "") == (1e-3, "m")
-    assert unit_policy.convert_value_to_base("test", "text", "") == ("test", "text")
+    assert unit_policy.convert_value_to_base(1, "mm") == (1e-3, "m")
+    assert unit_policy.convert_value_to_base("test", "text") == ("test", "text")
 
 
 def test_update_table(unit_policy):
@@ -46,3 +46,68 @@ def test_update_table(unit_policy):
 
     assert t["length"].values[0] == 1e-3
     assert t["length"].unit == "m"
+
+
+class myUnitPolicy(UnitPolicy):
+    """ Unit conversion based on TableColumn and TableName
+    """
+    def convert_value_to_base(self, value, unit: str) -> Tuple[Any, str]:
+        """ Here any unit converter can be integrated, pint, Unum &c.
+            This converter demonstrates the use of TableName and TableColumn
+        """
+        if self.TableName != "input_files_derived":
+            return value, unit
+
+        if self.TableColumn == "length":
+            if unit == "mm":
+                return value * 1e-3, "m"
+        elif self.TableColumn == "flt":
+            if unit == "m":
+                return value * 100, "cm"
+        print(f"{self.TableColumn} {value} {unit}")
+        return value, unit
+
+
+def test_UnitPolicy():
+    lines = (
+        dedent(
+            r"""
+    **input_files_derived;
+    all;
+    file_bytes;file_date;has_table;length;flt
+    -;text;onoff;mm;m;
+    15373;a;0;1;22.4;
+    15326;b;1;2;21.7;
+    """
+        )
+        .strip()
+        .split("\n")
+    )
+    t = make_table(lines, ";")
+
+    normalize_table_in_place(myUnitPolicy(), t)
+
+    assert t["length"].values[0] == 1e-3
+    assert t["length"].unit == "m"
+    assert t["flt"].values[0] == 2240.0
+    assert t["flt"].unit == "cm"
+
+    lines = (
+        dedent(
+            r"""
+    **input_2;
+    all;
+    file_bytes;file_date;has_table;length;
+    -;text;onoff;mm;
+    15373;a;0;1;
+    15326;b;1;2;
+    """
+        )
+        .strip()
+        .split("\n")
+    )
+    t_ident = make_table(lines, ";")
+    normalize_table_in_place(myUnitPolicy(), t_ident)
+
+    assert t_ident["length"].values[0] == 1
+    assert t_ident["length"].unit == "mm"
