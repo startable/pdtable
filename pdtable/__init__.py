ee0e1eb74e78afdc8b9e6a72e1778bac5548750c
JEACO <jeaco@orsted.dk>
2020-09-16 12:32:46 +0200

    Expose read_csv and read_excel in top-level __init__.py

--- pdtable/__init__.py
+++ pdtable/__init__.py
@@ -9,5 +9,6 @@ from .table_metadata import TableOrigin
 from .store import TableBundle
 from .units import UnitPolicy
 from .utils import read_bundle_from_csv, normalized_table_generator
+from .readers import read_csv, read_excel
 from .writers._csv import write_csv
 from .writers._excel import write_excel
