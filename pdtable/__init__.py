962923ee8770078109c9f650ec3341eee601cd7d
JEACO <jeaco@orsted.dk>
2020-09-16 12:34:10 +0200

    Fix the previous commit

--- pdtable/__init__.py
+++ pdtable/__init__.py
@@ -9,6 +9,7 @@ from .table_metadata import TableOrigin
 from .store import TableBundle
 from .units import UnitPolicy
 from .utils import read_bundle_from_csv, normalized_table_generator
-from .readers import read_csv, read_excel
+from .readers.read_csv import read_csv
+from .readers.read_excel import read_excel
 from .writers._csv import write_csv
 from .writers._excel import write_excel
