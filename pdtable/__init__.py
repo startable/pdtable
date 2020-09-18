2b3ebdf0ba62a3452bebe2850b05126d7ce20b3b
JEACO <jeaco@orsted.dk>
2020-09-18 22:19:14 +0200

    Expose more stuff in mid-level __init__.py files

--- pdtable/__init__.py
+++ pdtable/__init__.py
@@ -9,9 +9,10 @@ from .table_metadata import TableMetadata, TableOrigin
 from .store import TableBundle, BlockType, BlockGenerator
 from .units import UnitPolicy
 from .readers.parsers.FixFactory import FixFactory
-from .readers._csv import read_csv
-from .utils import read_bundle_from_csv, normalized_table_generator
+from .readers import read_csv
+from .readers import read_excel
 from .readers.parsers.blocks import JsonData
-from .writers._csv import write_csv
-from .writers._excel import write_excel
+from .writers import write_csv
+from .writers import write_excel
+from .utils import read_bundle_from_csv, normalized_table_generator
 from .json_utils import json_data_to_table, table_to_json_data
