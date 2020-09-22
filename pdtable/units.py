7d0657e2678b74df74353829c5018b99d710d989
thp-mac <thpetsen@gmail.com>
2020-09-22 14:23:33 +0200

    added TableName, TableColumn context to UnitPolicy + test demonstrating how to use these

--- pdtable/units.py
+++ pdtable/units.py
@@ -11,14 +11,30 @@ class UnitPolicy(ABC):
         """
         pass
 
+    @property
+    def TableColumn(self):
+        return self._table_column
+
+    @TableColumn.setter
+    def TableColumn(self,value):
+        self._table_column = value
+
+    @property
+    def TableName(self):
+        return self._table_name
+
+    @TableName.setter
+    def TableName(self,value):
+        self._table_name = value
+
 
 def normalize_table_in_place(unit_policy: UnitPolicy, table: TableType):
     """Apply unit policy, modifying table in-place"""
+    unit_policy.TableName = table.name
     for column in table.column_proxies:
         unit = column.unit
+        unit_policy.TableColumn = column.name
         new_values, new_unit = unit_policy.convert_value_to_base(column.values, unit)
         if not unit == new_unit:
             column.values = new_values
             column.unit = new_unit
-
-
