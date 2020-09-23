1e0b44f774496f8ab1879a874409eeef1c15da80
JEACO <jeaco@orsted.dk>
2020-09-23 12:23:19 +0200

    Forget about md and html, just make an ipynb notebook and execute it. And un-gitignore ipynb files.

--- scripts/make_pdtable_demo_notebook_markdown.bat
+++
@@ -1,8 +0,0 @@
-pushd %~dp0\..
-jupytext examples\pdtable_demo.py --to notebook
-rem jupytext examples\pdtable_demo.py --execute --to markdown --update-metadata "{""jupytext"": {""notebook_metadata_filter"":""-all""}}"
-jupyter nbconvert examples\pdtable_demo.ipynb --to notebook --execute --inplace
-jupyter nbconvert examples\pdtable_demo.ipynb --to markdown
-jupyter nbconvert examples\pdtable_demo.ipynb --to html
-rem --update-metadata "{""jupytext"": {""notebook_metadata_filter"":""-all""}}"
-popd
