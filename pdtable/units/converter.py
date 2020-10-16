05d51b3de848aeb645b3b0642ba488058f109423
JEACO <jeaco@orsted.dk>
2020-10-16 13:39:39 +0200

    Rename module 'pint' to '_pint' in attempt to solve build failing on ubuntu, maybe because of weird namespace thing?

--- pdtable/units/converter.py
+++ pdtable/units/converter.py
@@ -1,7 +1,7 @@
 """Default unit converter"""
 
 try:
-    from .pint import PintUnitConverter as DefaultUnitConverter
+    from ._pint import PintUnitConverter as DefaultUnitConverter
 except ImportError as err:
     raise ImportError(
         "Unable to import 'pint'. Please install 'pint' to use the default unit converter."
