40d0a097af814ac73c27b3cced6c21739c7d66a2
JEACO <jeaco@orsted.dk>
2020-10-24 23:54:36 +0200

    black

--- pdtable/units/converter.py
+++ pdtable/units/converter.py
@@ -2,6 +2,7 @@
 
 try:
     from ._pint import PintUnitConverter as DefaultUnitConverter
+
     # Singleton, for convenience.
     pint_converter = DefaultUnitConverter()
 
@@ -9,4 +10,3 @@ except ImportError as err:
     raise ImportError(
         "Unable to import 'pint'. Please install 'pint' to use the default unit converter."
     ) from err
-
