444a66f5a381c573a430f24a86a421a8e3da17aa
JEACO <jeaco@orsted.dk>
2020-11-29 22:02:08 +0100

    Add recommonmark to sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ author = 'Janus Halleløv Wesenberg, Jean-François Corbett'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'recommonmark',
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
 ]
