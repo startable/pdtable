ced53a758e2abf7e7703292eaac01c48dd85794d
Janus Wesenberg <jawes@orsted.dk>
2021-04-15 15:46:48 +0200

    Expose `load_files`
    Also add test for `.xlsx`-reader

--- pdtable/test/io/test_load.py
+++ pdtable/test/io/test_load.py
@@ -1,15 +1,22 @@
-
+import pytest
 from textwrap import dedent
 from pathlib import Path
-from pdtable.store import BlockType
+import re
 
+from pdtable.store import BlockType
 from pdtable import read_csv, Table
 from pdtable.table_origin import LoadItem, TableOrigin, LocationFile
 from pdtable.io.load import FilesystemLoader, load_files, LoadOrchestrator, make_location_trees
 
-def test_include():
-    input_folder = Path(__file__).parent / 'input/with_include'
-    res = {b.name: b for t, b in load_files(['/'], root_folder=input_folder, csv_sep=';') if t==BlockType.TABLE}
+
+@pytest.fixture
+def input_folder():
+    return Path(__file__).parent / 'input'
+
+
+def test_include(input_folder):
+    root_folder = input_folder /'with_include'
+    res = {b.name: b for t, b in load_files(['/'], root_folder=root_folder, csv_sep=';') if t==BlockType.TABLE}
 
     assert set(res.keys()) == {'bar_table', 'bar_abs_table'}
     input_location = res['bar_table'].metadata.origin.input_location
@@ -26,7 +33,7 @@ def test_include():
     location_trees = make_location_trees(res.values())
     tree_as_str = '\n'.join(str(n) for n in location_trees)
     reference_str = dedent(f"""
-    <root_folder: {input_folder}>
+    <root_folder: {root_folder}>
       Row 0 of 'input_foo.csv'
         bar_abs.csv
           **bar_abs_table
@@ -34,3 +41,12 @@ def test_include():
           **bar_table
     """).strip()
     assert tree_as_str == reference_str
+
+
+def test_excel(input_folder):
+    blocks = load_files(['/'], root_folder=input_folder, 
+                        file_name_pattern=re.compile(r'.*\.xlsx$'),
+                        sheet_name_pattern=re.compile(r'.*'))
+    res = [b for t, b in blocks  if t==BlockType.TABLE]
+
+    assert len(res) == 6
