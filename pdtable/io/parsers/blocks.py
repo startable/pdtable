0e7f7956d6e74b52525a0a0ec649081645b0b601
Jan Bielecki <jbiel@orsted.com>
2024-06-12 11:20:27 +0200

    init

--- pdtable/io/parsers/blocks.py
+++ pdtable/io/parsers/blocks.py
@@ -24,11 +24,10 @@ For each of these:
   - The original, raw cell grid, in case the user wants to do some low-level processing.
 
 """
-from abc import abstractmethod
 import itertools
+import logging
 import re
 from typing import Sequence, Optional, Tuple, Any, Iterable, List, Union, Dict
-from collections import defaultdict
 import pandas as pd
 import warnings
 
@@ -39,7 +38,6 @@ from pdtable.table_origin import (
     LocationSheet,
     NullLocationFile,
     TableOrigin,
-    InputIssue,
     InputIssueTracker,
     NullInputIssueTracker,
 )
@@ -51,6 +49,7 @@ from ...table_metadata import TableMetadata
 
 # Typing alias: 2D grid of cells with rows and cols. Intended indexing: cell_grid[row][col]
 CellGrid = Sequence[Sequence]
+logger = logging.getLogger(__name__)
 
 
 def make_metadata_block(cells: CellGrid, origin: Optional[str] = None, **_) -> MetadataBlock:
@@ -96,6 +95,18 @@ def parse_column_names(column_names_raw: Sequence[Union[str, None]]) -> List[str
     ]
 
 
+def _safe_strip(input_data: Any) -> str:
+    """
+    Save strip in cases where the input is not a string type.
+    """
+    # Data Validation
+    if not isinstance(input_data, str):
+        # Data Conversion
+        input_data = str(input_data)
+
+    return input_data.strip()
+
+
 def make_table_json_precursor(cells: CellGrid, origin, fixer:ParseFixer) -> Tuple[JsonDataPrecursor, bool]:
     """Parses cell grid into a JSON-like data structure but with some non-JSON-native values
 
@@ -118,7 +129,7 @@ def make_table_json_precursor(cells: CellGrid, origin, fixer:ParseFixer) -> Tupl
     fixer.table_name = table_name
 
     # internally hold destinations as json-compatible dict
-    destinations = {dest: None for dest in cells[1][0].strip().split(" ")}
+    destinations = {dest: None for dest in _safe_strip(cells[1][0]).split(" ")}
     table_is_empty = len(cells) < 3
     if table_is_empty:
         column_names = []
